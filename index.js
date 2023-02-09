11e90bc036aaca4e4fc8d9615600b45aef2741af
Taysa86 <Taysarolu86@gmail.com>
2023-02-08 21:29:32 -0600

    added question Array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the Title?', 'Describe Your Project.', 'Create a table of contents (optional)', 'What steps are required to install your project?', 'What are the instructions for using your project?', 'Who would you like to give credit to for helping create your project?', 'What badges would you like to include in your project?', 'What licenses are included in your project?', 'How can you test your project?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
