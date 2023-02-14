899d23f4f1a8d57b80c204bb1578a49528616fb3
Taysa86 <Taysarolu86@gmail.com>
2023-02-13 21:16:19 -0600

    cleaned up questios format adding type, name, and message objects

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = ['What is the Title?', 'Describe Your Project.', 'Create a table of contents (optional)', 'What steps are required to install your project?', 'What are the instructions for using your project?', 'Who would you like to give credit to for helping create your project?', 'What badges would you like to include in your project?', 'What licenses are included in your project?', 'How can you test your project?'];
-
+const questions = [
+      {
+        type: "input",
+        name: "title",
+        message: "What is the project title?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Enter project description",
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "Enter instructions for installation",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "Enter usage information",
+      },
+      {
+        type: "input",
+        name: "credits",
+        message: "Enter citations for credit",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "Enter test instructions",
+      },
+      {
+        type: "checkbox",
+        name: "license",
+        message: "Choose a license:",
+        choices: ['MIT', 'Mozilla', 'Apache', 'GPLv2'],
+      },
+      {
+        type: "input",
+        name: "userName",
+        message: "Enter your GitHub username",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email address",
+      },
+    ]; 
+    
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
